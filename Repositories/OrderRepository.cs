af24b51005fae4a0509f74d8dc132a0342ae5632
Gokul_Abisheak <gokul.abisheak@codeworxs.com>
2024-10-02 02:32:09 +0530

    completed order management

---
+++ Repositories/OrderRepository.cs
@@ -0,0 +1,77 @@
+namespace EAD.Repositories
+{
+    using EAD.Models;
+    using MongoDB.Driver;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly IMongoCollection<Order> _orders;
+
+        public OrderRepository(IMongoDatabase database)
+        {
+            _orders = database.GetCollection<Order>("Orders");
+        }
+
+        // Get all orders
+        public async Task<IEnumerable<Order>> GetAllOrdersAsync()
+        {
+            return await _orders.Find(_ => true).ToListAsync();
+        }
+
+        // Get order by ID
+        public async Task<Order> GetOrderByIdAsync(string id)
+        {
+            return await _orders.Find(o => o.Id == id).FirstOrDefaultAsync();
+        }
+
+        // Get orders by Customer ID
+        public async Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(string customerId)
+        {
+            return await _orders.Find(o => o.CustomerId == customerId).ToListAsync();
+        }
+
+        // Get orders by Vendor ID
+        public async Task<IEnumerable<Order>> GetOrdersByVendorIdAsync(string vendorId)
+        {
+            return await _orders.Find(o => o.VendorId == vendorId).ToListAsync();
+        }
+
+        // Create a new order
+        public async Task CreateOrderAsync(Order order)
+        {
+            if (string.IsNullOrEmpty(order.Id))
+            {
+                order.Id = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+            }
+
+            await _orders.InsertOneAsync(order);
+        }
+
+        // Update an existing order
+        public async Task<Order> UpdateOrderAsync(string id, Order order)
+        {
+            var result = await _orders.ReplaceOneAsync(o => o.Id == id, order);
+
+            if (result.MatchedCount == 0)
+            {
+                throw new Exception("Order not found for update.");
+            }
+
+            return order;
+        }
+
+        // Delete an order by ID
+        public async Task DeleteOrderAsync(string id)
+        {
+            var result = await _orders.DeleteOneAsync(o => o.Id == id);
+
+            if (result.DeletedCount == 0)
+            {
+                throw new Exception("Order not found for deletion.");
+            }
+        }
+    }
+}
--- obj/Debug/net6.0/EAD.AssemblyInfo.cs
+++ obj/Debug/net6.0/EAD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9d796d72e5b3a0a9b1fe1bf7eea7d760aef0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a60c406977cb8d3ac581234463610e8079c7177")]
 [assembly: System.Reflection.AssemblyProductAttribute("EAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
